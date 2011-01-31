a6381b9a8b3402de4092774b22b3cb2ae98fd1df
janisozaur <janisozaur@gmail.com>
2011-01-31 12:06:52 +0100

    add licensing information

--- sample.h
+++ sample.h
@@ -1,3 +1,26 @@
+/*
+ * stickman
+ * Copyright (C) 2011 Michał Janiszewski
+ *
+ * This work is a master thesis. Due to nature of law you are obliged to
+ * get a written permission prior to using any part of this work. If you
+ * do get a permission, you are able to use the work, which is thereafter
+ * licensed using GNU GPL v3 or (at your option) any later.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef SAMPLE_H
 #define SAMPLE_H
 
